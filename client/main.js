b7c55dffedd700b8ecf5fa52108a4b3f8a955f10
Mateo Ortiz Bedoya <matyu655@gmail.com>
2017-11-30 21:22:15 -0500

    First commit : First compiled load of meteor chat on Web platforms, Android, IOS

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+//importar vista HTML
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
+  // declaracion del contador
   this.counter = new ReactiveVar(0);
 });
 
@@ -14,9 +14,11 @@ Template.hello.helpers({
   },
 });
 
+//Eventos asociados
 Template.hello.events({
+  //seleccion del evento click
   'click button'(event, instance) {
-    // increment the counter when button is clicked
+    //Se incrementa el contador en +1 al hacer click
     instance.counter.set(instance.counter.get() + 1);
   },
 });
